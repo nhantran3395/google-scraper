d8759b54cdb80cb1c219f729d854c1fa161ba4ed
nhantran3395 <thanhnhan33.95@gmail.com>
2023-10-01 10:46:06 +0700

    feat/3: chore - add layout for login, register and home page

--- apps/web/src/components/layout/layout.tsx
+++ apps/web/src/components/layout/layout.tsx
@@ -4,10 +4,6 @@ export default function Layout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <main
-      className={"min-h-screen bg-slate-50 flex justify-center items-center"}
-    >
-      {children}
-    </main>
+    <main className={"min-h-screen bg-slate-50 flex flex-1"}>{children}</main>
   );
 }
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,3 +1,13 @@
-export default function ScrapperHome(): JSX.Element {
-  return <h1>Scrapper</h1>;
+import NavigationBar from "../components/navigation-bar";
+
+export default function ScrapperHome() {
+  return (
+    <div className="h-full w-full">
+      <NavigationBar />
+
+      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        {/* Your content */}
+      </div>
+    </div>
+  );
 }
