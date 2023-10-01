f4a8a6f55f69cbf32d5c70f1788e2fbfb5efbafc
nhantran3395 <thanhnhan33.95@gmail.com>
2023-10-01 10:55:37 +0700

    feat/3: feat - add link to register from login

--- apps/web/src/pages/login.tsx
+++ apps/web/src/pages/login.tsx
@@ -58,6 +58,16 @@ export default function LoginPage() {
             </button>
           </div>
         </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{" "}
+          <a
+            href="./register"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Register
+          </a>
+        </p>
       </div>
     </div>
   );
