b0c98d02ec85d228f5af5733898f7984293c169a
nhantran3395 <thanhnhan33.95@gmail.com>
2023-10-01 08:33:40 +0700

    feat/3: style - add css reset

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 
 import Layout from "../components/common/layout";
 
+import "../styles/reset.css";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
