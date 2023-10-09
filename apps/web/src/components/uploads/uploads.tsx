bef7c83076fed2e398f368ce47ec58cbb3b466c6
nhantran3395 <thanhnhan33.95@gmail.com>
2023-10-09 22:09:51 +0700

    task/46: chore - add new AuthenticatedLayout component and update other pages to use it

--- apps/web/src/components/uploads/uploads.tsx
+++ apps/web/src/components/uploads/uploads.tsx
@@ -1,33 +1,24 @@
-import useUser from "lib/use-auth.hook";
-import NavigationBar from "components/common/navigation-bar";
-import Dialog from "components/common/loading-indicator";
+import LoadingIndicator from "components/common/loading-indicator";
+import { AuthenticatedLayout } from "components/common/layout";
 
 import FileUpload from "./file-upload";
 import UploadGrid from "./upload-grid";
 import useFileUpload from "./use-file-upload.hook";
 
 export default function UploadsContainer() {
-  const { signOut } = useUser({
-    redirectTo: "/login",
-  });
-
   const fileUpload = useFileUpload();
 
   return (
-    <div className="h-full w-full">
-      <NavigationBar signOut={signOut} />
+    <AuthenticatedLayout>
+      <section>
+        <FileUpload {...fileUpload} />
+      </section>
 
-      <article className="mx-auto max-w-7xl py-6 px-6 sm:px-6 lg:px-8">
-        <section>
-          <FileUpload {...fileUpload} />
-        </section>
+      <section className="mt-4">
+        <UploadGrid />
+      </section>
 
-        <section className="mt-4">
-          <UploadGrid />
-        </section>
-      </article>
-
-      <Dialog isLoading={fileUpload.isProcessing} />
-    </div>
+      <LoadingIndicator isLoading={fileUpload.isProcessing} />
+    </AuthenticatedLayout>
   );
 }
